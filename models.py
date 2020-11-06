edit normalization / fix random seed

models.py
@@ -209,7 +209,7 @@ class DDDQNPolicy(Policy):
         self.t_step = (self.t_step + 1) % self.update_every
         if self.t_step == 0:
             # If enough samples are available in memory, get random subset and learn
-            if len(self.memory) > self.buffer_min_size and len(self.memory) > self.batch_size:
+            if len(self.memory) > self.buffer_min_size and len(self.memory) >= self.batch_size:
                 self._learn()
 
     def _learn(self):
@@ -317,7 +317,6 @@ class ReplayBuffer:
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
         experiences = random.sample(self.memory, k=self.batch_size)
-
         states = torch.from_numpy(self.__v_stack_impr([e.state for e in experiences if e is not None])) \
             .float().to(self.device)
         actions = torch.from_numpy(self.__v_stack_impr([e.action for e in experiences if e is not None])) \
@@ -339,7 +338,6 @@ class ReplayBuffer:
         #sub_dim = len(states[0][0]) if isinstance(states[0], Iterable) else 1
         #np_states = np.reshape(np.array(states), (len(states), sub_dim))
         np_states = np.array(states)
-        print(f"BUFFER SIZE: {np_states.shape}")
         # print("BUFFER:")
         # print(np_states)
         return np_states

observations.py
@@ -40,7 +40,9 @@ from railway_encoding import CellOrientationGraph
             - Packed paths and weights with numpy and only positions, remove direction
             - Add partial shortest path feature results to deviations path
             - Check normalization correctness
-            - Differentiate `inf` from maximum value in normalization
+            - Differentiate `inf` from maximum value in normalization (-2,2 ha senso?)
+            - Check distances correctness
+            - Represent distances as minimum distances inside an arc (not in the path before the node)
             - Handle observation not present at all (all -1 or all 0 ???)
 '''
 
@@ -136,12 +138,12 @@ class CustomObservation(ObservationBuilder):
                 )
                 self._update_data(handle, val[0], remaining_steps)
 
-                #print(f'Handle: {handle} Deviation Paths:')
+                # print(f'Handle: {handle} Deviation Paths:')
                 # for dev in val[1]:
                 #    print(dev.path)
 
-        #print(f'SHORTEST PATHS: \n {self._shortest_paths} \n')
-        #print(f'CUMULATIVE WEIGHTS: \n {self._shortest_cum_weights}\n')
+        # print(f'SHORTEST PATHS: \n {self._shortest_paths} \n')
+        # print(f'CUMULATIVE WEIGHTS: \n {self._shortest_cum_weights}\n')
         return super().get_many(handles)
 
     def _update_data(self, handle, prediction, remaining_steps):
@@ -221,7 +223,7 @@ class CustomObservation(ObservationBuilder):
             path = packed_paths[handle].tolist()[:len(path)]
             path_weights = packed_weights[handle].tolist()
 
-        #print(f'Handle {handle} -> Path {path}')
+        # print(f'Handle {handle} -> Path {path}')
         deadlocks, deadlock_distances = self.find_deadlocks(
             handle, path, path_weights, packed_paths, packed_weights
         )
@@ -263,14 +265,14 @@ class CustomObservation(ObservationBuilder):
             position = self.railway_encoding.get_agent_cell(agent)
             # Check if agent is not DONE_REMOVED -> position is None
             if position is not None:
-                node, remaining_distance = self.railway_encoding.next_node(
+                node, next_node_distance = self.railway_encoding.next_node(
                     position
                 )
                 nodes = self.railway_encoding.get_nodes((node[0], node[1]))
                 next_nodes = list(self.railway_encoding.graph.successors(node))
                 for other_node in nodes:
                     index = utils.get_index(path, other_node)
-                    if index:
+                    if index is not None:
                         if (other_node != node and
                             (len(next_nodes) > 1 or len(path) <= index + 1 or
                              (len(next_nodes) > 0 and next_nodes[0] != path[index + 1]))):
@@ -280,27 +282,31 @@ class CustomObservation(ObservationBuilder):
                         if malfunction > 0:
                             directions.append(directions[0] + 2)
                         break
-                if index:
+                if index is not None:
                     # For each computed direction (same and opposite)
                     for direction in directions:
                         num_agents[index:len(path), direction] += 1
                         value = (
                             cum_weights[index] +
-                            self.speed_data[agent].remaining
+                            self.speed_data[handle].remaining
                         )
                         # Same direction
                         if direction % 2 == 0:
                             value -= (
-                                remaining_distance *
+                                next_node_distance *
                                 self.speed_data[handle].times
                             )
                         # Other direction
                         else:
                             value += (
-                                remaining_distance *
+                                next_node_distance *
                                 self.speed_data[handle].times
                             )
                         # If malfunctioning
+
+                        ##########################
+                        # RIVEDERE DISTANZA IN CASO DI "MALFUNCTION"
+                        ##########################
                         if direction >= 2:
                             value = np.clip(malfunction - value, 0, None)
                         # Update distances
@@ -391,6 +397,7 @@ class CustomObservation(ObservationBuilder):
                         space = self.railway_encoding.graph.get_edge_data(
                             source, dest
                         )["weight"]
+                        init_space = space
                         # print(
                         #    f'Handle {handle} -> Deadlock Edge: {source}-{dest} Space: {space} \n My Turns {cum_weights[j],cum_weights[j+1]} \n Oth Turns {packed_weights[agent, i],packed_weights[agent, i+1]}')
                         deadlocks[j:len(path)] += 1
@@ -409,6 +416,9 @@ class CustomObservation(ObservationBuilder):
                                 packed_weights[agent, i] -
                                 abs(cum_weights[j])
                             ) / self.speed_data[agent].times
+                        if space < 0:
+                            print(init_space, space, self.last_nodes[handle][1], cum_weights[j],
+                                  packed_weights[agent, i])
                         crash_turn = np.ceil(
                             np.clip(cum_weights[j], 0, None) +
                             space / (1/self.speed_data[agent].times + 1 /
@@ -421,16 +431,33 @@ class CustomObservation(ObservationBuilder):
                     break
         return deadlocks, crash_turns
 
+    def normalize_distance(self, distances):
+        finite_distances = distances[
+            np.isfinite(distances)
+        ]
+        try:
+            max_distance = finite_distances.max()
+            min_distance = finite_distances.min()
+            if max_distance != min_distance:
+                distances = (
+                    (distances - min_distance) /
+                    (max_distance - min_distance)
+                )
+            else:
+                distances = distances / max_distance
+        except:
+            pass
+        distances[distances == np.inf] = 2
+        return distances
+
     def normalize(self, observation):
-        normalized_observation = np.full(
-            (self.max_depth, self.max_depth, self.FEATURES), -1, np.dtype('float32')
-        )
-        num_agents = observation[:, :, 0:4]
-        agent_distances = observation[:, :, 4:8]
-        target_distances = observation[:, :, 8]
-        c_nodes = observation[:, :, 9]
-        deadlocks = observation[:, :, 10]
-        deadlock_distances = observation[:, :, 11]
+        normalized_observation = observation.copy()
+        num_agents = normalized_observation[:, :, 0:4]
+        agent_distances = normalized_observation[:, :, 4:8]
+        target_distances = normalized_observation[:, :, 8]
+        c_nodes = normalized_observation[:, :, 9]
+        deadlocks = normalized_observation[:, :, 10]
+        deadlock_distances = normalized_observation[:, :, 11]
 
         # Normalize number of agents in path
         done_agents = sum([
@@ -447,30 +474,9 @@ class CustomObservation(ObservationBuilder):
         deadlocks /= remaining_agents
 
         # Normalize distances
-        finite_target_distances = target_distances[
-            np.isfinite(target_distances)
-        ]
-        try:
-            max_distance = finite_target_distances.max()
-            min_distance = finite_target_distances.min()
-            if max_distance != min_distance:
-                agent_distances = (
-                    (agent_distances - min_distance) /
-                    (max_distance - min_distance)
-                )
-                target_distances = (
-                    (target_distances - min_distance) /
-                    (max_distance - min_distance)
-                )
-                deadlock_distances = (
-                    (deadlock_distances - min_distance) /
-                    (max_distance - min_distance)
-                )
-        except:
-            pass
-        agent_distances[agent_distances == np.inf] = 1
-        target_distances[target_distances == np.inf] = 1
-        deadlock_distances[deadlock_distances == np.inf] = 1
+        agent_distances = self.normalize_distance(agent_distances)
+        target_distances = self.normalize_distance(target_distances)
+        deadlock_distances = self.normalize_distance(deadlock_distances)
 
         # Build the normalized observation
         normalized_observation[:, :, 0:4] = num_agents
@@ -479,5 +485,14 @@ class CustomObservation(ObservationBuilder):
         normalized_observation[:, :, 9] = c_nodes
         normalized_observation[:, :, 10] = deadlocks
         normalized_observation[:, :, 11] = deadlock_distances
-        normalized_observation[normalized_observation == -np.inf] = -1
+        normalized_observation[normalized_observation == -np.inf] = -2
+
+        # Check if the output is in range [-2, 2]
+        if not np.logical_and(
+            normalized_observation >= -2,
+            normalized_observation <= 2
+        ).all():
+            print(observation)
+            print(normalized_observation)
+
         return normalized_observation

train.py
@@ -1,5 +1,6 @@
 import os
 import random
+import sys
 from argparse import ArgumentParser
 from datetime import datetime
 
@@ -16,10 +17,10 @@ from flatland.utils.rendertools import RenderTool, AgentRenderVariant
 from predictions import ShortestPathPredictor
 from observations import CustomObservation
 from models import DDDQNPolicy
-from utils import Timer
+import utils
 
 
-RANDOM_SEED = 42
+RANDOM_SEED = 1
 
 
 def set_num_threads(num_threads):
@@ -31,16 +32,18 @@ def set_num_threads(num_threads):
     os.environ["MKL_NUM_THREADS"] = str(num_threads)
 
 
-def fix_random(seed):
+def tensorboard_log(writer, name, x, y):
     '''
-    Fix all the possible sources of randomness
+    Log the given x/y values to tensorboard
     '''
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.benchmark = False
-    torch.backends.cudnn.deterministic = True
+    if type(x) in (int, float, str):
+        writer.add_scalar(name, x, y)
+    else:
+        writer.add_scalar(f"{name}_min", np.min(x), y)
+        writer.add_scalar(f"{name}_max", np.max(x), y)
+        writer.add_scalar(f"{name}_mean", np.mean(x), y)
+        writer.add_scalar(f"{name}_std", np.std(x), y)
+        writer.add_histogram(name, np.array(x), y)
 
 
 def format_action_probabilities(action_probabilities):
@@ -67,10 +70,10 @@ def create_rail_env(args, observation_builder, env=""):
     else:
         rail_generator = sparse_rail_generator(
             max_num_cities=args.max_cities,
-            seed=RANDOM_SEED,
             grid_mode=args.grid,
             max_rails_between_cities=args.max_rails_between_cities,
             max_rails_in_city=args.max_rails_in_cities,
+            seed=RANDOM_SEED
         )
 
     # Initialize malfunctions
@@ -101,22 +104,9 @@ def create_rail_env(args, observation_builder, env=""):
         number_of_agents=args.num_trains,
         obs_builder_object=observation_builder,
         malfunction_generator=malfunctions,
-        remove_agents_at_target=True
+        remove_agents_at_target=True,
+        random_seed=RANDOM_SEED
     )
-
-
-def tensorboard_log(writer, name, x, y):
-    '''
-    Log the given x/y values to tensorboard
-    '''
-    if type(x) in (int, float, str):
-        writer.add_scalar(name, x, y)
-    else:
-        writer.add_scalar(f"{name}_min", np.min(x), y)
-        writer.add_scalar(f"{name}_max", np.max(x), y)
-        writer.add_scalar(f"{name}_mean", np.mean(x), y)
-        writer.add_scalar(f"{name}_std", np.std(x), y)
-        writer.add_histogram(name, np.array(x), y)
 
 
 def train_agents(args):
@@ -126,7 +116,7 @@ def train_agents(args):
     # Initialize threads and seeds
     set_num_threads(args.num_threads)
     if args.fix_random:
-        fix_random(RANDOM_SEED)
+        utils.fix_random(RANDOM_SEED)
 
     # Initialize predictor and observer
     predictor = ShortestPathPredictor(max_depth=args.max_depth)
@@ -136,9 +126,7 @@ def train_agents(args):
 
     # Setup the environments
     train_env = create_rail_env(args, observation_builder, env=args.train_env)
-    train_env.reset(regenerate_schedule=True, regenerate_rail=True)
     val_env = create_rail_env(args, observation_builder, env=args.val_env)
-    val_env.reset(regenerate_schedule=True, regenerate_rail=True)
 
     # Setup renderer
     if args.render:
@@ -188,7 +176,7 @@ def train_agents(args):
     training_id = now.strftime('%Y%m%d-%H%M%S')
 
     # Print initial training info
-    training_timer = Timer()
+    training_timer = utils.Timer()
     training_timer.start()
     print("\n🚉 Training {} trains on {}x{} grid for {} episodes, evaluating on {} episodes every {} episodes. Training id '{}'.\n".format(
         train_env.get_num_agents(),
@@ -202,16 +190,20 @@ def train_agents(args):
     # Do the specified number of episodes
     for episode in range(args.n_train_episodes + 1):
         # Initialize timers
-        step_timer = Timer()
-        reset_timer = Timer()
-        learn_timer = Timer()
-        inference_timer = Timer()
+        step_timer = utils.Timer()
+        reset_timer = utils.Timer()
+        learn_timer = utils.Timer()
+        inference_timer = utils.Timer()
 
         # Reset environment and renderer
         reset_timer.start()
-        obs, info = train_env.reset(
-            regenerate_rail=True, regenerate_schedule=True
-        )
+        if args.fix_random:
+            obs, info = train_env.reset(random_seed=RANDOM_SEED)
+        else:
+            obs, info = train_env.reset(
+                regenerate_rail=True, regenerate_schedule=True,
+                random_seed=utils.get_seed(train_env)
+            )
         reset_timer.end()
         if args.render:
             env_renderer.set_new_rail()
@@ -219,7 +211,7 @@ def train_agents(args):
         # Initialize data structures for training info
         score, steps = 0, 0
         actions_taken = []
-        agent_prev_obs = list(obs)
+        agent_prev_obs = dict(obs)
         agent_prev_action = [2] * args.num_trains
         update_values = [False] * args.num_trains
         action_count = [0] * action_size
@@ -256,7 +248,7 @@ def train_agents(args):
 
             # Update replay buffer and train agent
             for agent in train_env.get_agent_handles():
-                # Only learn from timesteps where somethings happened
+                # Only learn from timesteps where something happened
                 if update_values[agent] or done['__all__']:
                     learn_timer.start()
                     policy.step(
@@ -333,7 +325,7 @@ def train_agents(args):
         # Evaluate policy and log results at some interval
         if episode % args.checkpoint_interval == 0 and args.n_val_episodes > 0:
             scores, completions, val_steps = eval_policy(
-                eval_env, policy, train_params, obs_params
+                args, val_env, policy
             )
 
             # Save final validation scores
@@ -401,11 +393,13 @@ def train_agents(args):
         )
 
         # Log training time info to tensorboard
-        tensorboard_log(writer, "timer/reset", reset_timer.get(), episode)
-        tensorboard_log(writer, "timer/step", step_timer.get(), episode)
-        tensorboard_log(writer, "timer/learn", learn_timer.get(), episode)
+        tensorboard_log(writer, "utils.Timer/reset",
+                        reset_timer.get(), episode)
+        tensorboard_log(writer, "utils.Timer/step", step_timer.get(), episode)
+        tensorboard_log(writer, "utils.Timer/learn",
+                        learn_timer.get(), episode)
         tensorboard_log(
-            writer, "timer/total", training_timer.get_current(), episode
+            writer, "utils.Timer/total", training_timer.get_current(), episode
         )
 
         # Close tensorboard
@@ -424,6 +418,13 @@ def eval_policy(args, env, policy):
     for episode in range(args.n_val_episodes):
         score = 0.0
         final_step = 0
+        if args.fix_random:
+            obs, info = env.reset(random_seed=RANDOM_SEED)
+        else:
+            obs, info = env.reset(
+                regenerate_rail=True, regenerate_schedule=True,
+                random_seed=utils.get_seed(env)
+            )
         obs, info = env.reset(regenerate_rail=True, regenerate_schedule=True)
 
         # Do an episode