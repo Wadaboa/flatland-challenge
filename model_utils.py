:bug: fix loss computation

model_utils.py
@@ -30,16 +30,15 @@ def masked_argmax(vec, mask, dim=1, fill_value=np.nan):
     '''
     Argmax only on valid outputs
     '''
-
     masked_argmax_arr = ma.masked_array(
         vec, mask=np.invert(mask), fill_value=fill_value
     ).argmax(axis=dim, fill_value=fill_value)
 
+    # Argmax on masked arrays returns a plain numpy array,
+    # so we have to reshape it
     if dim > 0:
-        num_rows = vec.shape[0]
-        masked_argmax_arr = masked_argmax_arr.reshape(num_rows, 1)
-        
-        # masked_argmax_arr is a simple numpy array of indexes doesn't have a .mask property
-        # masked_argmax_arr[masked_argmax_arr.mask] = fill_value
+        new_shape = list(vec.shape)
+        new_shape[dim] = 1
+        masked_argmax_arr = masked_argmax_arr.reshape(tuple(new_shape))
 
     return masked_argmax_arr

policies.py
@@ -83,10 +83,9 @@ class DQNPolicy(Policy):
         Initialize DQNPolicy object
         '''
         super(DQNPolicy, self).__init__(state_size, choice_size)
-        assert (
-            isinstance(choice_selector, ActionSelector),
-            "The choice selection object must be an instance of ActionSelector"
-        )
+        assert isinstance(
+            choice_selector, ActionSelector
+        ), "The choice selection object must be an instance of ActionSelector"
 
         # Parameters
         self.state_size = state_size
@@ -117,7 +116,8 @@ class DQNPolicy(Policy):
             )
             self.loss = torch.tensor(0.0)
             self.memory = ReplayBuffer(
-                self.choice_size, self.PARAMETERS["batch_size"], self.PARAMETERS["buffer_size"], self.device
+                self.choice_size, self.PARAMETERS["batch_size"],
+                self.PARAMETERS["buffer_size"], self.device
             )
 
     def act(self, state, legal_choices):
@@ -170,7 +170,9 @@ class DQNPolicy(Policy):
 
         # Get expected Q-values from target model
         q_targets_next = torch.from_numpy(
-            self._get_q_targets_next(next_states, next_legal_choices)
+            self._get_q_targets_next(
+                next_states, next_legal_choices.cpu().numpy()
+            )
         ).to(self.device)
 
         # Compute Q-targets for current states
@@ -189,8 +191,6 @@ class DQNPolicy(Policy):
         # Update target network
         self._soft_update(self.qnetwork_local, self.qnetwork_target)
 
-    # TODO: To Review I don't like the fact that next_legal_choices must be moved to cpu
-    # to be converted into a numpy array to compute model_utils.masked_softmax
     def _get_q_targets_next(self, next_states, next_legal_choices):
         '''
         Get expected Q-values from target network
@@ -203,21 +203,20 @@ class DQNPolicy(Policy):
             q_targets_next = self.qnetwork_target(
                 next_states
             ).detach().cpu().numpy()
-            np_next_legal_choices = next_legal_choices.cpu().numpy()
 
             # Softmax Bellman
             if self.PARAMETERS["softmax_bellman"]:
                 return np.sum(
                     q_targets_next * model_utils.masked_softmax(
-                        q_locals_next, np_next_legal_choices
+                        q_locals_next, next_legal_choices
                     ), axis=1, keepdims=True
                 )
 
             # Standard Bellman
             best_choices = model_utils.masked_argmax(
-                q_targets_next, np_next_legal_choices
+                q_targets_next, next_legal_choices
             )
-            return q_targets_next[best_choices]
+            return np.take_along_axis(q_targets_next, best_choices, axis=1)
 
         def _dqn():
             q_targets_next = self.qnetwork_target(

train.py
@@ -268,6 +268,8 @@ def train_agents(args):
                         actions = observation_builder.railway_encoding.get_actions(
                             agent
                         )
+                        if len(actions) != 1:
+                            print(f"Rotte actions: {actions}")
                         assert len(actions) == 1
                         action = actions[0]
                 else:
@@ -410,13 +412,13 @@ def train_agents(args):
             writer, "choices/distribution", np.array(choices_taken), episode
         )
         tensorboard_log(
-            writer, "choices/left", choices_probs[env_utils.RailEnvChoices.CHOICE_LEFT], episode
+            writer, "choices/left", choices_probs[env_utils.RailEnvChoices.CHOICE_LEFT.value], episode
         )
         tensorboard_log(
-            writer, "choices/right", choices_probs[env_utils.RailEnvChoices.CHOICE_RIGHT], episode
+            writer, "choices/right", choices_probs[env_utils.RailEnvChoices.CHOICE_RIGHT.value], episode
         )
         tensorboard_log(
-            writer, "choices/stop", choices_probs[env_utils.RailEnvChoices.STOP], episode
+            writer, "choices/stop", choices_probs[env_utils.RailEnvChoices.STOP.value], episode
         )
 
         # Log training info to tensorboard
@@ -481,6 +483,7 @@ def eval_policy(args, railway_encoding, env, policy):
 
         # Compute agents with same source
         agents_with_same_start = env_utils.get_agents_same_start(env)
+
         # Do an episode
         for step in range(args.max_moves - 1):
             # Prioritize enter of faster agent in the environment
@@ -489,6 +492,7 @@ def eval_policy(args, railway_encoding, env, policy):
                     del agents_with_same_start[position][0]
                     for agent in agents_with_same_start[position]:
                         info['action_required'][agent] = False
+
             # Perform a step
             for agent in env.get_agent_handles():
                 if info['action_required'][agent]:
@@ -506,6 +510,8 @@ def eval_policy(args, railway_encoding, env, policy):
                         actions = railway_encoding.get_actions(
                             agent
                         )
+                        if len(actions) != 1:
+                            print(f"Rotte actions: {actions}")
                         assert len(actions) == 1
                         action = actions[0]
                 else: