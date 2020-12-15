:bug: small fix in normalization

utils.py
@@ -48,7 +48,7 @@ def min_max_scaling(values, lower, upper, under, over, known_min=None, known_max
         elif min_value != 0:
             values = values / min_value
         else:
-            values = under
+            values[:] = under
     except:
         pass
     values[values == -np.inf] = under